ff74ff70c3c90698ccb537185d840836a897ac4a
CodeCompleteYT <techstack.yt@gmail.com>
2023-11-05 01:18:32 +1100

    Add auth backend

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/vendor/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/components/shared/Navigation/Navbar/UserOptions/SignInButton.tsx
+++ src/components/shared/Navigation/Navbar/UserOptions/SignInButton.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { MdOutlineAccountCircle } from "react-icons/md";
 
 const SignInButton = () => {
   return (
-    <button className="flex flex-row gap-1 items-center border-[1px] border-slate-700 rounded-full overflow-hidden px-3 py-1.5 text-blue-400 cursor-pointer">
+    <button
+      className="flex flex-row gap-1 items-center border-[1px] border-slate-700 rounded-full overflow-hidden px-3 py-1.5 text-blue-400 cursor-pointer"
+      onClick={() => signIn("google")}
+    >
       <MdOutlineAccountCircle className="h-6 w-6" />
       Sign In
     </button>
